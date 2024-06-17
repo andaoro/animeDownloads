5c7c7e3ceaf278055bbe1483efc73aeccdd4c821
sistemas2 <andaoro1@gmail.com>
2024-06-17 16:58:56 -0500

    agregar iconos de acciones y tiempo en al hacer hover en inpput range

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,84 +1,101 @@
-import React, { MouseEventHandler, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import ReactPlayer from 'react-player'
 import './styles.css'
 import { FaPause, FaPlay } from 'react-icons/fa'
 import { IoVolumeHighSharp } from "react-icons/io5";
 import { IoMdVolumeOff } from "react-icons/io";
 import { BiExpand } from "react-icons/bi";
-import { FaGear } from "react-icons/fa6";
+import { MdForward10, MdReplay10 } from 'react-icons/md';
 
-
-
-
-
+type IconsVideo = 'pause' | 'play' | 'forward' | 'backward';
 
 type PlayerProps = {
     url: string,
     light?: string
-
-}
+};
 
 export const Player: React.FC<PlayerProps> = ({ url }) => {
-    const baseURL = import.meta.env.VITE_BASE_URL_MEDIA || ''
-    const volumeDefaultValue = 0.6
-    //HOOKS
+    const baseURL = import.meta.env.VITE_BASE_URL_MEDIA || '';
+    const volumeDefaultValue = 0.6;
+    const defaultIconType = 'pause';
 
-    ///STATES
-    const [playingVideo, setplayingVideo] = useState(false)
+    // STATES
+    const [playingVideo, setplayingVideo] = useState(false);
     const [volume, setVolume] = useState(volumeDefaultValue);
-    const [showForwardIcon, setShowForwardIcon] = useState(false);
-    const [showBackwardIcon, setShowBackwardIcon] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
-    const [visibleControls, setvisibleControls] = useState(false)
-    const [isMuted, setisMuted] = useState(false)
-    //const timeSpaceRef = useRef<number | null>(null)
-    //const [playBackRate, setplayBackRate] = useState(1)
+    const [visibleControls, setvisibleControls] = useState(false);
+    const [isMuted, setisMuted] = useState(false);
+    const [showEventIcon, setshowEventIcon] = useState(false);
+    const [iconType, seticonType] = useState<IconsVideo>(defaultIconType);
+    const [timeMouseMomentHover, settimeMouseMomentHover] = useState(0)
+    const [tooltipVisible, setTooltipVisible] = useState(false);
+    const [tooltipPosition, setTooltipPosition] = useState(0);
 
 
-    ///REFS
-    const playerRef = useRef<ReactPlayer>(null)
-    const progressBarRef = useRef<HTMLInputElement>(null)
-    const volumeBarRef = useRef<HTMLInputElement>(null)
+    // REFS
+    const playerRef = useRef<ReactPlayer>(null);
+    const progressBarRef = useRef<HTMLInputElement>(null);
+    const volumeBarRef = useRef<HTMLInputElement>(null);
     const controlsTimeoutRef = useRef<number | null>(null);
+    const optionIconsTimeoutRef = useRef<number | null>(null);
+    const tooltipRef = useRef<HTMLDivElement | null>(null)
 
-
-    //KEY FUNCTIONS
+    // KEY FUNCTIONS
     const handlePlayPause = () => {
-        setplayingVideo(prevPlaying => !prevPlaying)
-    }
+        setplayingVideo(prevPlaying => {
+            seticonType(prevPlaying ? 'play' : 'pause');
+            return !prevPlaying;
+        });
+        handleMouseMove();
+        setshowEventIcon(true);
+        if (optionIconsTimeoutRef.current) {
+            clearTimeout(optionIconsTimeoutRef.current);
+        }
+        optionIconsTimeoutRef.current = window.setTimeout(() => {
+            setshowEventIcon(false);
+        }, 700);
+    };
 
     const increaseVolume = () => {
         setVolume(prevVolume => Math.min(prevVolume + 0.1, 1));
-        validateMutedVideo()
+        validateMutedVideo();
     };
 
     const decreaseVolume = () => {
         setVolume(prevVolume => Math.max(prevVolume - 0.1, 0));
-        validateMutedVideo()
+        validateMutedVideo();
     };
 
     const seekForward = () => {
         if (playerRef.current) {
             const currentTime = playerRef.current.getCurrentTime();
             playerRef.current.seekTo(currentTime + 10);
-            setShowForwardIcon(true)
-            setTimeout(() => {
-                setShowForwardIcon(false)
-            }, (1000));
+            seticonType('forward');
+            setshowEventIcon(true);
+            if (optionIconsTimeoutRef.current) {
+                clearTimeout(optionIconsTimeoutRef.current);
+            }
+            optionIconsTimeoutRef.current = window.setTimeout(() => {
+                setshowEventIcon(false);
+            }, 700);
         }
-    }
+    };
 
     const seekBackForward = () => {
         if (playerRef.current) {
             const currentTime = playerRef.current.getCurrentTime();
-            playerRef.current.seekTo(currentTime - 11);
-            setShowForwardIcon(true)
-            setTimeout(() => {
-                setShowBackwardIcon(false)
-            }, 1000);
+            playerRef.current.seekTo(currentTime - 10);
+            seticonType('backward');
+            setshowEventIcon(true);
+            if (optionIconsTimeoutRef.current) {
+                clearTimeout(optionIconsTimeoutRef.current);
+            }
+            optionIconsTimeoutRef.current = window.setTimeout(() => {
+                setshowEventIcon(false);
+            }, 700);
         }
-    }
+    };
 
     const toggleFullScreen = () => {
         const playerWrapper = document.querySelector('.player-wrapper');
@@ -100,10 +117,7 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
         }
     };
 
-
-
-
-    //KEY EVENTS
+    // KEY EVENTS
     const handleKeyDown = (event: KeyboardEvent) => {
         switch (event.code) {
             case 'Space':
@@ -129,36 +143,15 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
         }
     };
 
-    /* const handleKeyDownSpace = (event: KeyboardEvent) => {
-        if (event.code === 'Space') {
-            timeSpaceRef.current = window.setTimeout(handleSpaceHold, 2000);
-        }
-    };
-
-    const handleSpaceHold = () => {
-        setplayBackRate(2)
-    };
-
-    const handleKeyUpSpace = (event: KeyboardEvent) => {
-        if (event.code === 'Space') {
-            if (timeSpaceRef.current) {
-                clearTimeout(timeSpaceRef.current);
-                timeSpaceRef.current = null;
-                setplayBackRate(1)
-            }
-        }
-    }; */
-
-
-    //CONTROL FUNCTIONS
+    // CONTROL FUNCTIONS
     const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = parseFloat(e.target.value);
         setVolume(value);
 
-        if (value == 0) {
-            setisMuted(true)
+        if (value === 0) {
+            setisMuted(true);
         } else {
-            setisMuted(false)
+            setisMuted(false);
         }
 
         if (volumeBarRef.current) {
@@ -170,17 +163,17 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
         }
     };
 
-    const handleProgress = (state: { played: number; playedSeconds: number; loadedSeconds: number }) => {
+    const handleProgress = (state: { played: number; playedSeconds: number; loadedSeconds: number; loaded: number }) => {
         setCurrentTime(state.playedSeconds);
+
         if (playerRef.current) {
-            setDuration(playerRef.current.getDuration())
+            setDuration(playerRef.current.getDuration());
         }
 
         if (progressBarRef.current) {
-            progressBarRef.current.style.setProperty('--range-value', String(state.played + 0.005));
+            progressBarRef.current.style.setProperty('--range-value', String(state.played + 0.00001));
+            progressBarRef.current.style.setProperty('--loaded-value', String(state.loaded + 0.00001)); // Actualiza el estilo del progreso de carga
         }
-
-
     };
 
     const handleSeekChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -201,8 +194,7 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
         return `${minutes}:${seconds}`;
     };
 
-    //MOUSE EVENTS
-
+    // MOUSE EVENTS
     const handleMouseOver = () => {
         setvisibleControls(true);
     };
@@ -221,34 +213,68 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
         }, 700);
     };
 
-    //VOLUME FUNCTION
+    const handleMouseMoveSeek = (e: React.MouseEvent<HTMLInputElement>) => {
+        const target = e.target as HTMLInputElement;
+        const { left, width } = target.getBoundingClientRect();
+        const { clientX } = e;
+        const position = clientX - left;
+        const value = Math.round((position / width) * parseInt(target.max));
 
+        settimeMouseMomentHover(value);
+        if (tooltipRef.current) {
+            tooltipRef.current.style.setProperty('--position-tooltip', String(`${position - 8}px`))
+        }
+        setTooltipPosition(position);
+        setTooltipVisible(true);
+    };
+
+    const handleMouseLeaveSeek = () => {
+        setTooltipVisible(false);
+    };
+
+    // VOLUME FUNCTION
     const mutedVideo = () => {
-        if (volume == 0) {
-            setVolume(volumeDefaultValue)
-            setisMuted(false)
+        if (volume === 0) {
+            setVolume(volumeDefaultValue);
+            setisMuted(false);
             if (volumeBarRef.current) {
                 volumeBarRef.current.style.setProperty('--range-valueV', String((volumeDefaultValue / 1) + 0.005));
             }
         } else {
-            setVolume(0)
-            setisMuted(true)
+            setVolume(0);
+            setisMuted(true);
             if (volumeBarRef.current) {
                 volumeBarRef.current.style.setProperty('--range-valueV', String((0 / 1) + 0.005));
             }
         }
-    }
+    };
 
     const validateMutedVideo = () => {
-        if (volume == 0) {
-            setisMuted(true)
+        if (volume === 0) {
+            setisMuted(true);
         } else {
-            setisMuted(false)
+            setisMuted(false);
         }
-    }
+    };
 
+    const showIconOption = (icon: IconsVideo = 'pause') => {
+        const defaultSize: number = 20;
+        const options = {
+            'pause': <FaPause size={defaultSize} />,
+            'play': <FaPlay size={defaultSize} />,
+            'forward': <MdForward10 size={defaultSize} />,
+            'backward': <MdReplay10 size={defaultSize} />
+        };
 
-    //USEEFFECT
+        return (
+            <div className={`absolute bottom-0 left-0 flex items-center justify-center w-full h-full pointer-events-none ${showEventIcon ? 'opacity-100' : 'opacity-0'} duration-300`}>
+                <div className='absolute w-16 h-16 rounded-full bg-gray-200/30'></div>
+                <span>{options[icon]}</span>
+            </div>
+        );
+    };
+
+    // USE EFFECT
     useEffect(() => {
         window.addEventListener('keydown', handleKeyDown);
 
@@ -256,28 +282,21 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
             volumeBarRef.current.style.setProperty('--range-valueV', String((volumeDefaultValue / 1) + 0.005));
         }
 
-        /* window.addEventListener('keydown', handleKeyDownSpace);
-        window.addEventListener('keyup', handleKeyUpSpace); */
-
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
-            /* window.removeEventListener('keydown', handleKeyDownSpace);
-            window.removeEventListener('keyup', handleKeyUpSpace); */
 
             if (controlsTimeoutRef.current) {
                 clearTimeout(controlsTimeoutRef.current);
             }
 
+            if (optionIconsTimeoutRef.current) {
+                clearTimeout(optionIconsTimeoutRef.current);
+            }
         };
     }, []);
 
     return (
-        <div
-            className='player-wrapper'
-            onMouseMove={handleMouseMove}
-        /* onMouseOver={handleMouseOver}
-        onMouseLeave={handleMouseLeave} */
-        >
+        <div className='player-wrapper' onMouseMove={handleMouseMove}>
             <div onClick={handlePlayPause}>
                 <ReactPlayer
                     className='react-player'
@@ -288,7 +307,6 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
                     playing={playingVideo}
                     volume={volume}
                     onProgress={handleProgress}
-                //playbackRate={playBackRate}
                 />
             </div>
 
@@ -301,12 +319,8 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
                         </div>
                         <div className='flex items-center group'>
                             <span className='cursor-pointer' onClick={mutedVideo}>
-                                {
-                                    isMuted ? <IoMdVolumeOff size={20} /> : <IoVolumeHighSharp size={20} />
-                                }
-
+                                {isMuted ? <IoMdVolumeOff size={20} /> : <IoVolumeHighSharp size={20} />}
                             </span>
-
                             <input
                                 type='range'
                                 min={0}
@@ -321,24 +335,13 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
                     </section>
 
                     <section className='flex mr-4 gap-x-4'>
-                        {/* OPCIONES EN CASO DE SER NECESARIAS */}
-                        {/* <div className='relative bg-red-500'>
-                            <span className='cursor-pointer'><FaGear size={20} /></span>
-                            
-                            <div className='absolute'>
-                                <p>Velocidad</p>
-                                <p>Descargar</p>
-                            </div>
-                        </div> */}
                         <span className='cursor-pointer'>
                             <BiExpand size={20} onClick={toggleFullScreen} />
                         </span>
                     </section>
-
-
                 </div>
 
-                <div className='flex items-center w-full pr-4'>
+                <div className='relative flex items-center w-full pr-4'>
                     <input
                         type='range'
                         min={0}
@@ -347,16 +350,27 @@ export const Player: React.FC<PlayerProps> = ({ url }) => {
                         onChange={handleSeekChange}
                         className='w-full mr-2 seek-slider'
                         ref={progressBarRef}
+                        onMouseMove={handleMouseMoveSeek}
+                        onMouseLeave={handleMouseLeaveSeek}
+
                     />
+
+                    {tooltipVisible && (
+                        <div
+                            className='seek-slider-tooltip'
+                            ref={tooltipRef}
+                        >
+                            {formatTime(timeMouseMomentHover)}
+                        </div>
+                    )}
 
                     <div className='hidden text-sm w-fit xl:flex gap-x-1 time'>
                         <span>{formatTime(currentTime)} </span> / <span> {formatTime(duration)}</span>
                     </div>
-
                 </div>
-
-
             </div>
+
+            {showIconOption(iconType)}
         </div>
-    )
-}
+    );
+};
